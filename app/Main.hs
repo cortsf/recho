680e6ab1e6653732cde24a9c7966cd5385f9ee43
Franco Cortesi <cortesifranco3@gmail.com>
2023-01-16 14:55:29 -0300

    wip - collect individual logs under ./logs/

--- app/Main.hs
+++ app/Main.hs
@@ -10,13 +10,18 @@ import Network.Wai.Handler.Warp
 import Servant
 import Network.Wai.Middleware.RequestLogger
 import Control.Monad.IO.Class
+import Data.Time (getCurrentTime)
+import Data.Time.Format.ISO8601 (iso8601Show)
 
 type Api = ReqBody '[JSON] Value :> Post '[JSON] String
 
 handler :: Value -> Handler String
 handler x = do
   liftIO $ putStrLn $ show x
-  pure "bye"
+  now <- liftIO getCurrentTime
+  liftIO $ putStrLn $ ("\n====== " <> iso8601Show now <> ":\n" <> show x)
+  liftIO $ writeFile ("logs/"<> iso8601Show now) (show x)
+  pure ("\n====== " <> iso8601Show now <> ":\n" <> show x)
 
 
 server :: Server Api
